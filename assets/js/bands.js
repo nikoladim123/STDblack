5b95458ccda9c12325ca559c542ca9bd452c4516
unknown <nikoladimitrijevic1991@gmail.com>
2019-08-08 19:56:07 +0200

    morph

---
+++ assets/js/bands.js
@@ -0,0 +1,22 @@
+var morphboxID = document.getElementById('morphboxID');
+var blackOverlayheader = document.getElementsByClassName('blackOverlayheader');
+
+morphboxID.addEventListener('click',()=>{
+  morphboxID.className += ' morphPClicked';
+  setTimeout(function () {
+    morphboxID.style.overflow = "visible";
+  }, 1000);
+  blackOverlayheader[0].style.display = 'block';
+  blackOverlayheader[0].style.opacity = '0.95';
+});
+
+blackOverlayheader[0].addEventListener('click',()=>{
+  morphboxID.className = ' morphBox';
+  morphboxID.style.overflow = "hidden";
+  blackOverlayheader[0].style.opacity = '0';
+  setTimeout(function () {
+    blackOverlayheader[0].style.display = 'none';
+  }, 1000);
+});
+
+console.log('hello');
