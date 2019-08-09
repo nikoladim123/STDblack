f7bf3f2d5b921f2cd763ac87fe443221ccbeadd0
unknown <nikoladimitrijevic1991@gmail.com>
2019-08-09 16:34:41 +0200

    morph

--- assets/js/bands.js
+++ assets/js/bands.js
@@ -1,23 +1,35 @@
+var bookAStudio = document.getElementsByClassName('bookAStudio');
 var morphboxID = document.getElementById('morphboxID');
 var blackOverlayheader = document.getElementsByClassName('blackOverlayheader');
 var bannerButtonContainer = document.getElementsByClassName('bannerButtonContainer');
 
-morphboxID.addEventListener('click',()=>{
-  morphboxID.className += ' morphPClicked';
-  blackOverlayheader[0].style.display = 'block';
-  blackOverlayheader[0].style.opacity = '0.35';
-  bannerButtonContainer[0].style.opacity = '1'
-  morphboxID.style.height = "29vw";
+var tick = 1;
+bookAStudio[0].addEventListener('click',()=>{
+  if (tick) {
+    setTimeout(function () {
+      tick=0;
+    }, 1100);
+    morphboxID.className += ' morphPClicked';
+    blackOverlayheader[0].style.display = 'block';
+    setTimeout(function () {
+      blackOverlayheader[0].style.opacity = '0.8';
+    }, 100);
+    bannerButtonContainer[0].style.opacity = '1'
+    morphboxID.style.height = "29vw";
+  }
 });
 
 blackOverlayheader[0].addEventListener('click',()=>{
-  morphboxID.className = ' morphBox';
-  blackOverlayheader[0].style.opacity = '0';
-  setTimeout(function () {
-    blackOverlayheader[0].style.display = 'none';
-  }, 1000);
-  bannerButtonContainer[0].style.opacity = '0'
-  morphboxID.style.height = "6vw";
+  if(!tick){
+    morphboxID.className = ' morphBox';
+    blackOverlayheader[0].style.opacity = '0';
+    setTimeout(function () {
+      blackOverlayheader[0].style.display = 'none';
+      tick = 1;
+    }, 1100);
+    bannerButtonContainer[0].style.opacity = '0'
+    morphboxID.style.height = "6vw";
+  }
 });
 
 console.log('hello');
