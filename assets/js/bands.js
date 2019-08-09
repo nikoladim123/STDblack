5c1ceaf8df67acfd0232449834b894d6f11fcc25
unknown <nikoladimitrijevic1991@gmail.com>
2019-08-09 15:10:58 +0200

    morph change

--- assets/js/bands.js
+++ assets/js/bands.js
@@ -1,22 +1,23 @@
 var morphboxID = document.getElementById('morphboxID');
 var blackOverlayheader = document.getElementsByClassName('blackOverlayheader');
+var bannerButtonContainer = document.getElementsByClassName('bannerButtonContainer');
 
 morphboxID.addEventListener('click',()=>{
   morphboxID.className += ' morphPClicked';
-  setTimeout(function () {
-    morphboxID.style.overflow = "visible";
-  }, 1000);
   blackOverlayheader[0].style.display = 'block';
-  blackOverlayheader[0].style.opacity = '0.75';
+  blackOverlayheader[0].style.opacity = '0.35';
+  bannerButtonContainer[0].style.opacity = '1'
+  morphboxID.style.height = "29vw";
 });
 
 blackOverlayheader[0].addEventListener('click',()=>{
   morphboxID.className = ' morphBox';
-  morphboxID.style.overflow = "hidden";
   blackOverlayheader[0].style.opacity = '0';
   setTimeout(function () {
     blackOverlayheader[0].style.display = 'none';
   }, 1000);
+  bannerButtonContainer[0].style.opacity = '0'
+  morphboxID.style.height = "6vw";
 });
 
 console.log('hello');
