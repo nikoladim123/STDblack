b6eb5235beca45f0c5291978302bc1111995b029
unknown <nikoladimitrijevic1991@gmail.com>
2019-08-12 17:42:43 +0200

    preload video

---
+++ assets/js/loadingJS.js
@@ -0,0 +1,67 @@
+// loading video functions
+var loadingVid = document.getElementsByClassName('loadingVid');
+var videoForLoad = document.getElementsByClassName('videoForLoad');
+var theStu = document.getElementsByClassName('theStu');
+var talent = document.getElementsByClassName('talent');
+
+
+
+
+// videoForLoad[0].onloadeddata = function() {
+//   theStu[0].style.fontSize = '2.5vw';
+//     setTimeout(function () {
+//       theStu[0].style.filter = 'blur(50px)';
+//     }, 1500);
+//     setTimeout(function () {
+//       loadingVid[0].style.opacity = '0';
+//       setTimeout(function () {
+//         loadingVid[0].style.display = 'none';
+//       }, 1000);
+//     }, 3000);
+// }
+
+
+function videoLoadFun() {
+  theStu[0].style.fontSize = '3vw';
+    setTimeout(function () {
+      theStu[0].style.filter = 'blur(50px)';
+    }, 1500);
+    setTimeout(function () {
+      loadingVid[0].style.opacity = '0';
+      setTimeout(function () {
+        loadingVid[0].style.display = 'none';
+      }, 1000);
+    }, 3000);
+}
+
+
+videoForLoad[0].addEventListener("playing", function() {
+    console.log("[Playing] loading of video");
+    if ( videoForLoad[0].readyState == 4 ) {
+        console.log("[Finished] loading of video");
+        videoLoadFun();
+    }
+});
+videoForLoad[0].addEventListener("suspend", function(e) {
+    console.log("[Suspended] loading of video");
+    if ( videoForLoad[0].readyState == 4 ) {
+        console.log("[Finished] loading of video");
+        videoLoadFun();
+    }
+});
+
+
+
+
+// videoForLoad[0].addEventListener("loadeddata", function () {
+//   theStu[0].style.fontSize = '2.5vw';
+//     setTimeout(function () {
+//       theStu[0].style.filter = 'blur(50px)';
+//     }, 1500);
+//     setTimeout(function () {
+//       loadingVid[0].style.opacity = '0';
+//       setTimeout(function () {
+//         loadingVid[0].style.display = 'none';
+//       }, 1000);
+//     }, 3000);
+// });
