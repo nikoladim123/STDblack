832bbb605c407ef975a644a4637d85a00759319b
unknown <nikoladimitrijevic1991@gmail.com>
2019-08-14 19:59:08 +0200

    stupid fix

--- assets/js/yt.js
+++ assets/js/yt.js
@@ -1,32 +1,93 @@
-// Load the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// // Load the IFrame Player API code asynchronously.
+// var tag = document.createElement('script');
+// tag.src = "https://www.youtube.com/player_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//
+// // Replace the 'ytplayer' element with an <iframe> and
+// // YouTube player after the API code downloads.
+// var player;
+// function onYouTubePlayerAPIReady() {
+//   player = new YT.Player('featuredVid', {
+//     height: '360',
+//     width: '640',
+//     videoId: 'dy_TXvnF9RU',
+//     playerVars: {
+//       rel: 0,
+//       showinfo: 0,
+//       controls: 0,
+//       modestbranding: 1,
+//       autohide:0,
+//       cc_load_policy:0
+//     }
+//   });
+// }
+//
+// //  custom
+//
 
-// Replace the 'ytplayer' element with an <iframe> and
-// YouTube player after the API code downloads.
-var playerFeaturedBands;
-function onYouTubePlayerAPIReady() {
-  playerFeaturedBands = new YT.Player('featuredVid', {
-    height: '360',
-    width: '640',
-    videoId: 'dy_TXvnF9RU',
-    playerVars: { rel: 0, showinfo: 0, controls: 0}
-  });
-}
+// }
+//
 
-//  custom
 
-var ytFeaturedSec = document.getElementsByClassName('ytFeaturedSec');
 
-function playYTfeaturedBands() {
-  if(ytFeaturedSec[0].getBoundingClientRect().top < window.innerHeight){
-    console.log('yt feature if');
-    playerFeaturedBands.playVideo();
+
+
+  // 2. This code loads the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // 3. This function creates an <iframe> (and YouTube player)
+  //    after the API code downloads.
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('featuredVid', {
+      height: '390',
+      width: '640',
+      videoId: 'dy_TXvnF9RU',
+      playerVars: {
+        rel: 0,
+        showinfo: 0,
+        controls: 0,
+        modestbranding: 1,
+        autohide:0,
+        cc_load_policy:0,
+        mute: 1
+      },
+      events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+      }
+    });
   }
-}
+  // 4. The API will call this function when the video player is ready.
+  function onPlayerReady(event) {
+    console.log('ready');
+    // event.target.playVideo();
+  }
 
-window.addEventListener('scroll',()=>{
-  playerFeaturedBands.playVideo()
-})
+  // 5. The API calls this function when the player's state changes.
+  //    The function indicates that when playing a video (state=1),
+  //    the player should play for six seconds and then stop.
+  var done = false;
+  function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+      setTimeout(stopVideo, 6000);
+      done = true;
+    }
+  }
+  function stopVideo() {
+    player.stopVideo();
+  }
+
+  // var ytFeaturedSec = document.getElementsByClassName('ytFeaturedSec');
+  // //
+  // function playYTfeaturedBands() {
+  //   if(ytFeaturedSec[0].getBoundingClientRect().top < window.innerHeight){
+  //     console.log('yt feature if');
+  //     player.playVideo();
+  //   }
+  // }
