7c254c2b967b9fd6d59b1eb01120ae4b6cf959e2
unknown <nikoladimitrijevic1991@gmail.com>
2019-08-16 18:47:14 +0200

    add footer on all pages

--- assets/js/addYourSpace.js
+++ assets/js/addYourSpace.js
@@ -9,7 +9,6 @@ for (var i = 0; i < spacerLeftMenuSelection.length; i++) {
 var customI = 0;
 function customLoop(e) {
   if(customI < spacerLeftMenuSelection.length){
-    console.log(spacerLeftMenuSelection[customI].dataset.selected);
     spacerLeftMenuSelection[customI].dataset.selected = 0;
     customI++;
     customLoop(e);
@@ -17,5 +16,4 @@ function customLoop(e) {
     customI = 0;
   };
   e.target.dataset.selected = 1;
-    console.log(e.target.dataset.selected);
 }
