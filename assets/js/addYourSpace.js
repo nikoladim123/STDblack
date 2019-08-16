3631d281a0266149b562c2f2682f74241cd8c9b7
unknown <nikoladimitrijevic1991@gmail.com>
2019-08-16 22:58:02 +0200

    add your studio

--- assets/js/addYourSpace.js
+++ assets/js/addYourSpace.js
@@ -1,4 +1,5 @@
 var spacerLeftMenuSelection = document.getElementsByClassName('spacerLeftMenuSelection');
+var rightContent = document.getElementsByClassName('rightContent');
 
 for (var i = 0; i < spacerLeftMenuSelection.length; i++) {
   spacerLeftMenuSelection[i].addEventListener('click',(e)=>{
@@ -10,10 +11,12 @@ var customI = 0;
 function customLoop(e) {
   if(customI < spacerLeftMenuSelection.length){
     spacerLeftMenuSelection[customI].dataset.selected = 0;
+    rightContent[customI].style.display = 'none';
     customI++;
     customLoop(e);
   }else{
     customI = 0;
   };
   e.target.dataset.selected = 1;
+  rightContent[e.target.dataset.menusel].style.display = 'block';
 }
