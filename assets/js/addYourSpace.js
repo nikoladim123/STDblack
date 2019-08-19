f7d99f8d2e0c13fd1b91d7623cf6a180663821eb
unknown <nikoladimitrijevic1991@gmail.com>
2019-08-19 16:18:03 +0200

    left side menu continue button fix

--- assets/js/addYourSpace.js
+++ assets/js/addYourSpace.js
@@ -1,5 +1,6 @@
 var spacerLeftMenuSelection = document.getElementsByClassName('spacerLeftMenuSelection');
 var rightContent = document.getElementsByClassName('rightContent');
+var continueButton = document.getElementsByClassName('continueButton');
 
 for (var i = 0; i < spacerLeftMenuSelection.length; i++) {
   spacerLeftMenuSelection[i].addEventListener('click',(e)=>{
@@ -19,4 +20,22 @@ function customLoop(e) {
   };
   e.target.dataset.selected = 1;
   rightContent[e.target.dataset.menusel].style.display = 'block';
+}
+
+
+
+for (var i = 0; i < continueButton.length; i++) {
+  continueButton[i].addEventListener('click',(e)=>{
+    continueButtonFun(e);
+  })
+}
+
+function continueButtonFun(e) {
+  for (var i = 0; i < spacerLeftMenuSelection.length; i++) {
+    rightContent[i].style.display = 'none';
+    spacerLeftMenuSelection[i].dataset.selected = 0;
+  }
+  rightContent[e.target.dataset.buttonselection].style.display = 'block';
+  spacerLeftMenuSelection[e.target.dataset.buttonselection].dataset.selected = 1;
+  console.log(e.target.dataset.buttonselection);
 }
