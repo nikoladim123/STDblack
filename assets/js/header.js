6e6e8aed29ca1bc8e80ed6ebf3cf6bdf43292051
unknown <nikoladimitrijevic1991@gmail.com>
2019-09-16 21:12:40 +0200

    std register form

--- assets/js/header.js
+++ assets/js/header.js
@@ -12,19 +12,43 @@ userDropDownBox[0].addEventListener('mouseleave',()=>{
 });
 
 
-var register = document.getElementsByClassName('register');
+var logIn = document.getElementsByClassName('logIn');
 var loginOverlay = document.getElementsByClassName('loginOverlay');
 var closeBoxLogIn = document.getElementsByClassName('closeBoxLogIn');
 
-register[0].addEventListener('click',(e)=>{
-  e.preventDefault();
-  loginOverlay[0].style.display = 'block';
-  setTimeout(function () {
-    loginOverlay[0].style.opacity = '1';
-  }, 50);
-});
+if(typeof logIn[0] !== 'undefined'){
+  logIn[0].addEventListener('click',(e)=>{
+    e.preventDefault();
+    loginOverlay[0].style.display = 'block';
+    setTimeout(function () {
+      loginOverlay[0].style.opacity = '1';
+    }, 50);
+  });
+}
+
 
 closeBoxLogIn[0].addEventListener('click',()=>{
   loginOverlay[0].style.display = 'none';
   loginOverlay[0].style.opacity = '0';
+})
+
+// register
+var register = document.getElementsByClassName('register');
+var registerOverlay = document.getElementsByClassName('registerOverlay');
+var registerOverlayCloseButton = document.getElementsByClassName('registerOverlayCloseButton');
+
+if(typeof register[0] !== 'undefined'){
+  register[0].addEventListener('click',(e)=>{
+    e.preventDefault();
+    registerOverlay[0].style.display = 'flex';
+    setTimeout(function () {
+      registerOverlay[0].style.opacity = '1';
+    }, 50);
+  });
+}
+
+registerOverlayCloseButton[0].addEventListener('click',()=>{
+  registerOverlay[0].style.display = 'none';
+  registerOverlay[0].style.opacity = '0';
+  console.log(213);
 })
