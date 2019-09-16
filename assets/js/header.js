59c427e54a1bae1f39540b8caabc333c15b34713
unknown <nikoladimitrijevic1991@gmail.com>
2019-09-16 20:00:16 +0200

    login/regg

--- assets/js/header.js
+++ assets/js/header.js
@@ -9,4 +9,22 @@ userDropDownArrow[0].addEventListener('click',()=>{
 userDropDownBox[0].addEventListener('mouseleave',()=>{
   userDropDownBox[0].style.maxHeight = '0vw';
   userDropDownBox[0].style.border = '0px solid white';
+});
+
+
+var register = document.getElementsByClassName('register');
+var loginOverlay = document.getElementsByClassName('loginOverlay');
+var closeBoxLogIn = document.getElementsByClassName('closeBoxLogIn');
+
+register[0].addEventListener('click',(e)=>{
+  e.preventDefault();
+  loginOverlay[0].style.display = 'block';
+  setTimeout(function () {
+    loginOverlay[0].style.opacity = '1';
+  }, 50);
+});
+
+closeBoxLogIn[0].addEventListener('click',()=>{
+  loginOverlay[0].style.display = 'none';
+  loginOverlay[0].style.opacity = '0';
 })
