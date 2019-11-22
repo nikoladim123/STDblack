566bca47b1f3f5448cbc9106ed2bb489b9d46cc3
unknown <nikoladimitrijevic1991@gmail.com>
2019-11-22 02:44:10 +0100

    add blog

---
+++ assets/js/blog.js
@@ -0,0 +1,21 @@
+var sixImageDiv = document.getElementsByClassName('sixImageDiv');
+var sixImageBlackDivInside = document.getElementsByClassName('sixImageBlackDivInside');
+var instaBG = document.getElementsByClassName('instaBG');
+
+// six image flexbox
+// six image flexbox
+// six image flexbox
+for (var i = 0; i < sixImageDiv.length; i++) {
+  sixImageDiv[i].addEventListener('mouseenter',(e)=>{
+    e.target.childNodes[3].style.left = '0vw';
+    e.target.childNodes[3].style.bottom = '0vw';
+    setTimeout(function () {
+      e.target.childNodes[3].style.opacity = '1';
+    }, 100);
+  });
+  sixImageDiv[i].addEventListener('mouseleave',(e)=>{
+    e.target.childNodes[3].style.left = '-15.793vw';
+    e.target.childNodes[3].style.bottom = '-13.653vw';
+    e.target.childNodes[3].style.opacity = '0';
+  })
+}
