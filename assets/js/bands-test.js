f71d37155c30e4b71b5288d5da4150da5dff668e
unknown <nikoladimitrijevic1991@gmail.com>
2019-11-27 20:57:37 +0100

    new test bands

---
+++ assets/js/bands-test.js
@@ -0,0 +1,46 @@
+// ajax
+var ytVideos = [];
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    console.log(this.responseText)
+    ytVideos = JSON.parse(this.responseText);
+    galOneFun()
+    // init gallery
+  }
+};
+xhttp.open("GET", "http://149.56.98.59:3055/yt", true);
+xhttp.send();
+
+
+// JSX content;
+// create content
+var sectionGridYT = document.getElementsByClassName('sectionGridYT');
+
+// 4 different class names used for boxes /in each loop different class is used
+var gridBoxClassNames = ['gridSpanThree','gridSpanTwoLong','gridSpanTwoShort','gridSpanSmall'];
+
+// inition each Box every new loop for different content
+var gridBox;
+function initiateGridBox(i) {
+  gridBox = `
+  <div class="gridBoxContent ">
+    <h2>FULL PARTY <br>
+    IN BOSTON </h2>
+    <hr>
+    <p>Local Boys were fantastic <br>
+    At the Burkley Performance Center</p>
+  </div>
+  <iframe src="${ytVideos.ytVids[i]}?rel=0&amp;controls=1&amp;showinfo=0&amp;modestbranding=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+  `;
+}
+
+function galOneFun() {
+  for (var i = 0; i < ytVideos.ytVids.length; i++) {
+    initiateGridBox(i);
+    var newDiv = document.createElement('div');
+    newDiv.className = gridBoxClassNames[i%4];
+    newDiv.innerHTML = gridBox;
+    sectionGridYT[0].append(newDiv);
+  }
+}
