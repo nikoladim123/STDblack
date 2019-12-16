2c4b874341440311162d102d520849865679af42
unknown <nikoladimitrijevic1991@gmail.com>
2019-12-16 20:08:24 +0100

    dance academy

--- assets/js/danceAcademy.js
+++ assets/js/danceAcademy.js
@@ -4,22 +4,62 @@ var cameraBox = document.getElementsByClassName('cameraBox');
 var customCamera = document.getElementsByClassName('customCamera');
 var cameraInput = document.getElementsByClassName('cameraInput');
 
-danceBaner[0].addEventListener('mouseenter',()=>{
-  customCamera[0].style.opacity = '0.8';
-});
+// danceBaner[0].addEventListener('mouseenter',()=>{
+//   customCamera[0].style.opacity = '0.8';
+// });
+//
+// danceBaner[0].addEventListener('mouseleave',()=>{
+//   customCamera[0].style.opacity = '0';
+// });
+//
+// cameraBox[0].addEventListener('mouseenter',()=>{
+//   customCamera[0].style.opacity = '1';
+// });
+//
+// cameraBox[0].addEventListener('click',()=>{
+//   uploadBanerFixedContainer[0].style.display = 'flex';
+// });
+//
+// uploadBanerFixedContainer[0].addEventListener('click',(e)=>{
+//   uploadBanerFixedContainer[0].style.display = 'none';
+// })
 
-danceBaner[0].addEventListener('mouseleave',()=>{
-  customCamera[0].style.opacity = '0';
-});
 
-cameraBox[0].addEventListener('mouseenter',()=>{
-  customCamera[0].style.opacity = '1';
-});
+// thumbnail
+var thumbnail = document.getElementsByClassName('thumbnail');
+var bannerImg = document.getElementsByClassName('bannerImg');
 
-cameraBox[0].addEventListener('click',()=>{
-  uploadBanerFixedContainer[0].style.display = 'flex';
-});
 
-uploadBanerFixedContainer[0].addEventListener('click',(e)=>{
-  uploadBanerFixedContainer[0].style.display = 'none';
+function thumbnailClick(e) {
+  bannerImg[0].style.opacity = 0;
+  setTimeout(function () {
+
+    bannerImg[0].src = `assets/images/danceAcadaemy/banerImages/${e.dataset.thumbnail}.jpg`
+    bannerImg[0].dataset.newSet = e.dataset.thumbnail;
+  }, 300);
+  console.log(e.dataset.thumbnail);
+}
+
+bannerImg[0].addEventListener('load',(e)=>{
+    bannerImg[0].style.opacity = '1';
+})
+
+for (var i = 0; i < thumbnail.length; i++) {
+  thumbnail[i].addEventListener('click',(e)=>{
+    thumbnailClick(e.target);
+  })
+}
+
+// bannerImg
+bannerImg[0].addEventListener('click',(e)=>{
+  bannerImg[0].style.opacity = 0;
+  setTimeout(function () {
+    if (parseInt(e.target.dataset.newSet) <= 4) {
+      bannerImg[0].src = `assets/images/danceAcadaemy/banerImages/${parseInt(e.target.dataset.newSet)+1}.jpg`;
+      bannerImg[0].dataset.newSet = parseInt(e.target.dataset.newSet)+1;
+    }else{
+      bannerImg[0].src = `assets/images/danceAcadaemy/banerImages/1.jpg`;
+      bannerImg[0].dataset.newSet = 1;
+    }
+  }, 300);
 })
